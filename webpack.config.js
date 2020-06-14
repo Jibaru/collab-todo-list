7142ced4775e208a29cf116dd2b45309a8cf35d5
Ignacio <ignacioruedaboada@hotmail.com>
2020-06-14 18:01:28 -0500

    Delete webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
